4ebbbec319688f5781fe6c6a58c52156e8a53e1a
Neha985-ui <neharana3359@gmail.com>
2025-01-15 07:57:13 +0000

    my first eda

---
+++ .ipython/profile_default/startup/00-env.py
@@ -0,0 +1,17 @@
+# *WARNING* Do not change this file as it may break your notebooks
+
+import os
+os.environ['HOME'] = '/home/104c0660-1be7-404b-8861-82ee06d5989e'
+os.environ['LC_CTYPE'] = 'en_US.utf-8'
+os.environ['JAVA_HOME'] = '/usr/lib/jvm/default-java'
+os.environ['TERM'] = 'xterm-256color'
+os.environ['PYTHONSTARTUP'] = '/home/104c0660-1be7-404b-8861-82ee06d5989e/.pythonstartup.py'
+os.environ['PYTHONANYWHERE_DOMAIN'] = 'anacondaapps.cloud'
+os.environ['PYTHONANYWHERE_SITE'] = 'nb.anaconda.cloud'
+os.environ['ANACONDA_ASSISTANT_API_URL'] = 'https://assistant.anaconda.cloud'
+os.environ['ANACONDA_ASSISTANT_ENVIRONMENT_TYPE'] = 'cloud-notebooks-prod'
+os.environ['ANACONDA_PROJECTS_DIR'] = 'anaconda_projects'
+os.environ['http_proxy'] = 'http://proxy.server:3128'
+os.environ['https_proxy'] = 'http://proxy.server:3128'
+os.environ['no_proxy'] = 'localhost,127.0.0.1,localaddress,.localdomain.com,/var/run/docker.sock'
+os.environ['API_TOKEN'] = '78b35750a3a2508acbfdb45232171dcf4c51cc34'
